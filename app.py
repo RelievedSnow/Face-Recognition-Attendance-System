7590d72733edc9a09ac140b08f31d394288ef718
Neelam Savardekar <119112527+RelievedSnow@users.noreply.github.com>
2024-09-01 13:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,35 +145,39 @@ def start():
 # This function will run when we add a new user.
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    newusername = request.form['newusername']
-    newuserid = request.form['newuserid']
-    userimagefolder = 'static/faces/'+newusername+'_'+str(newuserid)
-    if not os.path.isdir(userimagefolder):
-        os.makedirs(userimagefolder)
-    i, j = 0, 0
-    cap = cv2.VideoCapture(0)
-    while 1:
-        _, frame = cap.read()
-        faces = extract_faces(frame)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
-            cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
-            if j % 5 == 0:
-                name = newusername+'_'+str(i)+'.jpg'
-                cv2.imwrite(userimagefolder+'/'+name, frame[y:y+h, x:x+w])
-                i += 1
-            j += 1
-        if j == nimgs*5:
-            break
-        cv2.imshow('Adding new User', frame)
-        if cv2.waitKey(1) == 10:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    print('Training Model')
-    train_model()
-    names, rolls, times, l = extract_attendance()
-    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg())
+    try:
+        newusername = request.form['newusername']
+        newuserid = request.form['newuserid']
+        userimagefolder = 'static/faces/'+newusername+'_'+str(newuserid)
+        if not os.path.isdir(userimagefolder):
+            os.makedirs(userimagefolder)
+        i, j = 0, 0
+        cap = cv2.VideoCapture(0)
+        while 1:
+            _, frame = cap.read()
+            faces = extract_faces(frame)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
+                cv2.putText(frame, f'Images Captured: {i}/{nimgs}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+                if j % 5 == 0:
+                    name = newusername+'_'+str(i)+'.jpg'
+                    cv2.imwrite(userimagefolder+'/'+name, frame[y:y+h, x:x+w])
+                    i += 1
+                j += 1
+            if j == nimgs*5:
+                break
+            cv2.imshow('Adding new User', frame)
+            if cv2.waitKey(1) == 10:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+        print('Training Model')
+        train_model()
+        names, rolls, times, l = extract_attendance()
+        return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg())
+    except Exception as e:
+        print(f"Error: {e}")
+        return str(e)
 
 # Our main function which runs the Flask App
 if __name__ == '__main__':
